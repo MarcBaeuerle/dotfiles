466229d1f624ee92375b5bf69487bb8c608eb1f4
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-04-22 16:01:22 -0700

    reverted to old harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -628,19 +628,18 @@ require('lazy').setup({
   },
   { -- Easy tabs
     'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local harpoon = require 'harpoon'
-      harpoon:setup()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
 
-      vim.keymap.set("n", "<leader>q", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<leader>w", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>q", mark.add_file)
+      vim.keymap.set("n", "<leader>w", ui.toggle_quick_menu)
 
-      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>1", function () ui.nav_file(1) end)
+      vim.keymap.set("n", "<leader>2", function () ui.nav_file(2) end)
+      vim.keymap.set("n", "<leader>3", function () ui.nav_file(3) end)
+      vim.keymap.set("n", "<leader>4", function () ui.nav_file(4) end)
     end,
   },
   {
